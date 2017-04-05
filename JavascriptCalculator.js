bbbb9b9013e18ad4b181a571193dacf2dc0ff3ae
Trey <treyvis@gmail.com>
2017-04-05 08:56:10 -0600

    Making JavaScriptCalculator an object contructor

--- JavascriptCalculator.js
+++ JavascriptCalculator.js
@@ -4,70 +4,71 @@ Accept input
 	operators
 	equals
 */
+function JavascriptCalculator() {
+	var formula = "";
+	var input = "";
+	var operators = ["+","-","*","/"];
 
-var formula = "";
-var input = "";
-var operators = ["+","-","*","/"];
+	this.numberInput = function (number) {
+		if (typeof number === "number" &&
+			number >= 0 &&
+			number < 10) {
 
-function numberInput(number) {
-	if (typeof number === "number" &&
-		number >= 0 &&
-		number < 10) {
+			if (number === 0 &&
+				input === "") {
+				return;
+			}
 
-		if (number === 0 &&
-			input === "") {
-			return;
+			input += number;
 		}
+	};
 
-		input += number;
-	}
-}
+	this.operatorInput = function (operator) {
+		if (typeof operator === "string" && operators.indexOf(operator) !== -1){
 
-function operatorInput(operator) {
-	if (typeof operator === "string" && operators.indexOf(operator) !== -1){
-
-		console.log("Valid Operator");
-		if (input !== ""){
-			formula += input + operator;
-			input = "";
-		} else if (formula !== "" && operators.indexOf(formula[formula.length - 1]) === -1){
-			formula += operator;
+			console.log("Valid Operator");
+			if (input !== ""){
+				formula += input + operator;
+				input = "";
+			} else if (formula !== "" && operators.indexOf(formula[formula.length - 1]) === -1){
+				formula += operator;
+			}
 		}
-	}
-}
+	};
 
-function dotInput(){
-	if (input.indexOf(".") === -1){
-		if (input === ""){
-			input += "0.";
-		} else {
-			input += ".";
+	this.dotInput = function (){
+		if (input.indexOf(".") === -1){
+			if (input === ""){
+				input += "0.";
+			} else {
+				input += ".";
+			}
 		}
-	}
-}
+	};
 
-function equal(){
-	formula += input;
-	if (operators.indexOf(formula[formula.length -1]) !== -1){
-		formula = formula.slice(0, formula.length - 1);
-	}
-	formula = eval(formula);
-	input = "";
-}
-
-function clear(){
-	if (input !== "") {
+	this.equal = function (){
+		formula += input;
+		if (operators.indexOf(formula[formula.length -1]) !== -1){
+			formula = formula.slice(0, formula.length - 1);
+		}
+		formula = eval(formula);
 		input = "";
-	} else if (formula !== "" && operators.indexOf(formula[formula.length - 1]) !== -1) {
-		formula = formula.slice(0, formula.length - 1);
-	} else {
-		formula = "";
-	}
-}
+	};
 
-function clearAll(){
-	input = "";
-	formula = "";
+	this.clear = function (){
+		if (input !== "") {
+			input = "";
+		} else if (formula !== "" && operators.indexOf(formula[formula.length - 1]) !== -1) {
+			formula = formula.slice(0, formula.length - 1);
+		} else {
+			formula = "";
+		}
+	};
+
+	this.clearAll = function (){
+		input = "";
+		formula = "";
+	};
 }
 
 /*
